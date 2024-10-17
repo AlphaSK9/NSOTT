8b5cb169ba1c007e189234ffd9d8e1122dd44b6b
AlphaSK9 <sks400@icloud.com>
2024-10-16 19:10:45 -0700

    NSOTT

---
+++ scripts.js
@@ -0,0 +1,28 @@
+/* NSOT */
+
+function soonAlert() {
+ var myText = "Review Coming Soon!";
+ alert(myText);
+}
+function navigate() {
+    document.getElementById("iframe").src
+        = "https://www.youtube.com/embed/4eyMiA954n0?si=52ZQ4GcXKi_YKDmi&rel=0";
+ 
+}
+function navigate2() {
+    document.getElementById("iframe").src
+        = "https://www.youtube.com/embed/4OHPBdU3GaY?si=jTVx6sPpf4xstwQP&rel=0";
+}
+function navigate3() {
+    document.getElementById("iframe").src
+        = "https://www.youtube.com/embed/sCoMBXKHP0Y?si=ruZJwKzZ0SyJWd3z&rel=0";
+}
+function navigate4() {
+    document.getElementById("iframe").src
+        = "https://www.youtube.com/embed/VaLX5sQNx6U?si=jri2OaVKfVPF7kC-&rel=0";
+}
+function navigate5() {
+    document.getElementById("iframe").src
+        = "https://www.youtube.com/embed/1AfjLoVa3LI?si=kFRUswnRP7KxzZV_&rel=0";
+}
+
